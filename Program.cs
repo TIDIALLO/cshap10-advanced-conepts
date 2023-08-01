0ad313fd94585db3dd5eef322fa521378faa6366
Tidiane Diallo <diallotidiane014@gmail.com>
2023-08-01 14:53:29 +0000

    first commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿
+//A normal delegate
+Calculator c = new Calculator(Add);
+c(4, 6);
+c = new Calculator(Mul);
+c(4, 6);
+
+static void Add(int a, int b)
+{
+    Console.WriteLine(a+b);
+}
+static void Mul(int a, int b)
+{
+    Console.WriteLine(a * b);
+}
+
+delegate void Calculator(int x, int y);
+
