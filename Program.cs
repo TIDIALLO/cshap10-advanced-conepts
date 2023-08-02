3b3cec81184066b4dbce84bfdae551165b1f07cf
Tidiane Diallo <diallotidiane014@gmail.com>
2023-08-02 09:56:42 +0000

    using delegate

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿Func<double, double> square = Square;
+﻿/*Func<double, double> square = Square;
 Console.WriteLine($"square : {square(2)}");
 
 Func<double, double> cubic = Cubic;
@@ -9,3 +9,16 @@ static double Square(double x) => Math.Pow(x, 2);
 static double Cubic(double x) => Math.Pow(x, 3);
 static double Two() => 2;
 static void Shox() => Console.WriteLine("hello");
+*/
+Console.WriteLine("=======================================");
+
+StringDelegate sd = ToLowererCase;
+sd("TTTTTTTT"); 
+sd = ToUpperCase;
+sd("bgbirùrg");
+
+static void ToUpperCase(string text) => Console.WriteLine(text.ToUpper());
+static void ToLowererCase(string text) => Console.WriteLine(text.ToLower());
+
+public delegate void StringDelegate(string s);
+
